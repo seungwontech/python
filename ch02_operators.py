f70acabd4862d306c713834d4f485dae9a038752
sw.lee <swlee173@gmail.com>
2022-08-07 20:09:58 +0900

    ch02_연산자

--- ch02_operators.py
+++ ch02_operators.py
@@ -3,23 +3,56 @@
 print(1 + 2)
 
 # - 빼기 4
-print(5-1)
+print(5 - 1)
 
 # * 곱하기 108
-print(54*2)
+print(54 * 2)
 
 # / 나누기 27.0
-print(54/2)
+print(54 / 2)
 
 # // 몫 27
-print(54//2)
+print(54 // 2)
 
 # % 나머지 0 ,1
-print(54%2)
-print(55%2)
+print(54 % 2)
+print(55 % 2)
 
 # 거듭 제곱 25, 4
 print(5 ** 2)
 print(2 ** 2)
 # ---------------------------------
+
+
 # 비교 연산자
+a_num = 5
+b_num = 2
+# 기호: >
+# 의미: 크다
+# 결과: True
+print(a_num > b_num)
+
+# 기호: >=
+# 의미: 크거나 같다
+# 결과: True
+print(a_num >= b_num)
+
+# 기호: <
+# 의미: 작다
+# 결과: False
+print(a_num < b_num)
+
+# 기호: <=
+# 의미: 작거나 같다
+# 결과: False
+print(a_num <= b_num)
+
+# 기호: ==
+# 의미: 같다
+# 결과: False
+print(a_num == b_num)
+
+# 기호: !=
+# 의미: 같지 않다
+# 결과: True
+print(a_num != b_num)
