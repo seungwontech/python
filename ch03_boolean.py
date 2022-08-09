283e5a67254bea4293cbbd4756f3c7a631148305
sw.lee <swlee173@gmail.com>
2022-08-09 19:34:02 +0900

    ch02_불리안

--- ch03_boolean.py
+++ ch03_boolean.py
@@ -3,13 +3,18 @@ b = " "
 c = ""
 
 print(bool(a))
+# 결과: True
 print(bool(b))
+# 결과: True
 print(bool(c))
-
+# 결과: False
 d = 1
 e = -1
 f = 0
 
 print(bool(d))
+# 결과: True
 print(bool(e))
+# 결과: True
 print(bool(f))
+# 결과: False
