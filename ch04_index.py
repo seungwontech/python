7ad3d5f8f0227f9873f7f64f32cfa211b219073c
sw.lee <swlee173@gmail.com>
2022-08-10 19:11:29 +0900

    ch04_인덱스

---
+++ ch04_index.py
@@ -0,0 +1,20 @@
+a = 'python'
+# 0 or -6 value : p
+# 1 or -5 value : y
+# 2 or -4 value : t
+# 3 or -3 value : h
+# 4 or -2 value : o
+# 5 or -1 value : n
+
+print(a[0:6])
+# 결과: python
+print(a[1:6])
+# 결과: ython
+print(a[-6:])
+# 결과: python
+print(a[-6:6])
+# 결과: python
+print(a[1:5])
+# 결과: ytho
+print(a[0:5])
+# 결과: pytho
