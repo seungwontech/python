8c21f7efe6137d1748a8ee9a8ee6f6c26b3f52ef
sw.lee <swlee173@gmail.com>
2022-08-18 23:14:27 +0900

    ch06_포맷

---
+++ ch06_format.py
@@ -0,0 +1,9 @@
+python ='파이썬'
+java ='자바'
+
+print('오늘은 {}과 {} 공부할게요'.format(python,java))
+# 결과: 오늘은 파이썬과 자바 공부할게요
+print('오늘은 {1}과 {0} 공부할게요'.format(python,java))
+# 결과: 오늘은 자바과 파이썬 공부할게요
+print(f'오늘은 {python}과 {java} 공부할게요')
+# 결과: 오늘은 파이썬과 자바 공부할게요
