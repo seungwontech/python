bb494cbacdc30f0c7c840e69572a2130ae8a01f6
sw.lee <swlee173@gmail.com>
2022-08-20 16:25:16 +0900

    ch07_리스트

--- ch07_list.py
+++ ch07_list.py
@@ -20,3 +20,42 @@ b_list = ['y', 'z']
 a_list.extend(b_list)
 print(a_list)
 # 결과: ['b', 'c', 'd', 'y', 'z']
+
+# 원하는 위치에 값 추가
+a_list.insert(0, 'k')
+print(a_list)
+# 결과: ['k', 'b', 'c', 'd', 'y', 'z']
+
+# 원하는 위치 또는 마지막의 값 삭제
+# 원하는 위치
+a_list.pop(0)
+print(a_list)
+# 결과: ['b', 'c', 'd', 'y', 'z']
+
+# 마지막의 값 삭제
+a_list.pop()
+print(a_list)
+# 결과: ['b', 'c', 'd', 'y']
+
+# 모든 값 삭제
+a_list.clear()
+print(a_list)
+# 결과: []
+
+# 값의 순서대로 정렬
+c_list = ['b', 'a', 'c', 'a']
+c_list.sort()
+print(c_list)
+# 결과: ['a', 'a', 'b', 'c']
+
+# 순서 뒤집기
+c_list.reverse()
+print(c_list)
+# 결과: ['c', 'b', 'a', 'a']
+# 어떤 값이 몇개 있는지
+print(c_list.count('a'))
+# 결과: 2
+
+# 어떤 값이 어디에 위치했는지
+print(c_list.index('b'))
+# 결과: 1
