ced7ec47bf63b466f3b98b31c051c3e6555d691a
sw.lee <swlee173@gmail.com>
2022-09-19 22:46:05 +0900

    ch15_함수

--- ch15_function.py
+++ ch15_function.py
@@ -2,9 +2,36 @@
 def price():
     print('1000원 입니다.')
 
-
 print('이건 얼마인가요?')
 price()
 # 결과
 # 이건 얼마인가요?
 # 1000원 입니다.
+
+def test_function():
+    print('호출')
+
+test_function()
+# 결과: 호출
+
+def test_function(i):
+    print(i, '번째 호출')
+
+for i in range(5):
+    test_function(i + 1)
+
+
+# 사용하지 않은 예제
+def test_sum(a, b):
+    a + b
+
+
+print(test_sum(1, 2))
+# 결과: None
+
+# 사용한 예제
+def test_sum(a, b):
+    return a + b
+
+print(test_sum(1, 2))
+# 결과: 3
