be82af7fa4a75699a8f65cf5d1493e6133e8af02
seungwonlee <swlee173@gmail.com>
2022-11-07 11:35:14 +0900

    백준_영수증

--- baekjoon/ex25304.py
+++ baekjoon/ex25304.py
@@ -5,8 +5,8 @@ for i in range(1, n + 1):
     a, b = map(int, input().split())
     x -= a * b
 
-print('Yes' if x == 0 else 'No')
 if x == 0:
     print('Yes')
 else:
     print('No')
+# print('Yes' if x == 0 else 'No')
