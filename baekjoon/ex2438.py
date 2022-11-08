27712f90151f5be659fad3369b57875384fb3c6c
seungwonlee <swlee173@gmail.com>
2022-11-08 09:45:30 +0900

    백준_별찍기-1

--- baekjoon/ex2438.py
+++ baekjoon/ex2438.py
@@ -1,3 +1,9 @@
 n = int(input())
-for i in range(1, n + 1):
-    print('*' * i)
+for i in range(n):
+    for j in range(i + 1):
+        print('*', end='')
+    print()
+
+# n = int(input())
+# for i in range(1, n + 1):
+#    print('*' * i)
