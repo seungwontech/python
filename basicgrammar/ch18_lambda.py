48d6f58db032f605c6f95e8a94435a14dd97a8f2
seungwonlee <swlee173@gmail.com>
2022-11-22 15:42:37 +0900

    ch18_람다표현식

--- basicgrammar/ch18_lambda.py
+++ basicgrammar/ch18_lambda.py
@@ -6,3 +6,21 @@ print(test_sum(1, 2))
 # 결과: 3
 print((lambda a, b: a + b)(3, 4))
 # 결과: 7
+
+
+def test_calc(x):
+    return x * x
+
+
+for i in range(1, 5):
+    print(test_calc(i))
+
+# 1
+# 4
+# 9
+# 16
+
+print(list(map(test_calc, range(1, 5))))
+# [1, 4, 9, 16]
+print(list(map(lambda x: x * x, range(1, 5))))
+# [1, 4, 9, 16]
