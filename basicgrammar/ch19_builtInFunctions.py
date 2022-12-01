c74377adc990dccb232739c83a6ee1b5ea9ae68e
seungwonlee <swlee173@gmail.com>
2022-12-01 12:52:39 +0900

    ch19_내장함수

--- basicgrammar/ch19_builtInFunctions.py
+++ basicgrammar/ch19_builtInFunctions.py
@@ -1,6 +1,28 @@
-# sum()
-sum_result = sum([1, 2, 3])
-print(sum_result)
+# list
+list = [1, 2, 4]
+list_sum_result = sum(list)
+print(list_sum_result)
+# 결과: 7
+
+# tuple
+tuple = (1, 3, 6)
+tuple_sum_result = sum(tuple)
+print(tuple_sum_result)
+# 결과: 10
+
+# set
+set = {1, 2, 5}
+set_sum_result = sum(set)
+print(set_sum_result)
+# 결과: 8
+
+# dictionary
+dictionary = {1: 2, 13: 4}
+dictionary_keys_sum_result = sum(dictionary.keys())
+print(dictionary_keys_sum_result)
+# 결과: 14
+dictionary_values_sum_result = sum(dictionary.values())
+print(dictionary_values_sum_result)
 # 결과: 6
 
 # min(), max()
