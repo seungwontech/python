46a51acf4f0c3fc9728756606204252fdc7fd4a1
seungwonlee <swlee173@gmail.com>
2022-12-01 11:21:05 +0900

    ch19_내장함수

---
+++ basicgrammar/ch19_builtInFunctions.py
@@ -0,0 +1,20 @@
+# sum()
+sum_result = sum([1, 2, 3])
+print(sum_result)
+# 결과: 6
+
+# min(), max()
+min_result = min(3, 1, 2)
+print(min_result)
+# 결과: 1
+max_result = max(3, 1, 2)
+print(max_result)
+# 결과: 3
+
+# sorted()
+sorted_result = sorted([5, 34, 2, 6, 78, 1])
+print(sorted_result)
+# 결과: [1, 2, 5, 6, 34, 78]
+sorted_reverse_result = sorted([5, 34, 2, 6, 78, 1], reverse=True)
+# 결과: [78, 34, 6, 5, 2, 1]
+print(sorted_reverse_result)
