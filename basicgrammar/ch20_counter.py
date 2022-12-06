5a700b8bd045ac256b9b857d3be724834f5d8cc9
seungwonlee <swlee173@gmail.com>
2022-12-06 17:30:43 +0900

    ch20_Counter함수

--- basicgrammar/ch20_counter.py
+++ basicgrammar/ch20_counter.py
@@ -5,3 +5,8 @@ test_list = ['a', 'b', 'c', 'a']
 print(Counter(test_list))
 # 결과: Counter({'a': 2, 'b': 1, 'c': 1})
 
+test_count = Counter('Hello python')
+print(test_count)
+# 결과: Counter({'l': 2, 'o': 2, 'H': 1, 'e': 1, ' ': 1, 'p': 1, 'y': 1, 't': 1, 'h': 1, 'n': 1})
+
+
