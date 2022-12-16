d41d3d16aab29b899b57144b8b943006dfb49813
seungwonlee <swlee173@gmail.com>
2022-12-16 17:42:46 +0900

    그리드 알고리즘 곱하기 혹은 더하기

---
+++ algorithm/grid/ex03.py
@@ -0,0 +1,10 @@
+n = input()
+result = int(n[0])
+for i in range(1, len(n)):
+    num = int(n[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
